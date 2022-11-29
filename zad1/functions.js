e1225ed1a0c05cf66127a43cbaafecc0e9fb416a
krzychiu <krzysztof.lingoo@gmail.com>
2022-11-29 09:02:47 +0100

    initial commint

---
+++ zad1/functions.js
@@ -0,0 +1,13 @@
+
+function isOdd(x) {
+    return x % 2 !== 0;
+}
+
+function isEven(x) {
+    return x % 2 === 0;
+}
+
+
+console.log(isOdd(10))
+console.log(isEven(10))
+console.log(isEven(1.22))
